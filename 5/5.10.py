d4c33eed5667c85f50d57c970e6554081e20167c
zizhuoliu2013 <zizhuoliu2013@u.northwestern.edu>
2018-02-08 18:35:38 -0600

    update

--- 5/5.10.py
+++ 5/5.10.py
@@ -1,15 +1,17 @@
 def apply_permutation(perm, A):
     for i in range(len(A)):
-        next_pos = i
-        while perm[next_pos] >= 0:
-            A[i], A[perm[next_pos]] = A[perm[next_pos]], A[i]
-            temp = perm[next_pos]
-            perm[next_pos] -= len(perm)
-            next_pos = temp
+        next = i
+        while perm[next] >= 0 & next >= 0 :
+            A[next], A[perm[next]] = A[perm[next]], A[next]
+            temp = perm[next]
+            perm[next] -= len(perm)
+            next = temp
+            print(perm)
+            print(A)
     perm[:] = [a + len(perm) for a in perm]
 
 if __name__ == '__main__':
-    perm = [3,1,2,0]
-    A = ['a','b','c','d']
+    perm = [3,1,4,0,2]
+    A = [0,1,2,3,4]
     apply_permutation(perm,A)
     print(A)
